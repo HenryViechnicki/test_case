48b8fd7319ed9e0344b7cd5b362cd9ff4f15c961
ElliottPaschane <165910043+ElliottPaschane@users.noreply.github.com>
2024-04-03 13:57:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,23 @@ function writeData(){
 			fs.writeFile(output_name, csv, (err) => {
 			if (err) throw err;
 			});
-}
+	}
+
+function buttonClick() {
+			writeData();
+			//var fs = require('fs');
+			//const tableData = ["Elliott", "39, -77"];
+			//const csv = tableData.map(row => row.join(',')).join('\n');
+			//let output_name = "userData.csv";
+			//fs.writeFile(output_name, csv, (err) => {
+			//if (err) throw err;
+			//});
+			//let data = "run you stupid program";
+			//fs.writeFile('user_data.txt', data, (err) => {if (err) throw err;});
+			var code = prompt("Enter the code:");
+			if (code === "1112") {
+				window.location.href = "playing.html";
+			}else {
+				alert("Wrongish code. Please try again.");
+			}
+		}
